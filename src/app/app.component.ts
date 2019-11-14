55b422e27609e6a18d9b675e61056ab4fb861930
unknown <olga1gordeyeva@gmail.com>
2019-11-14 18:49:45 -0500

    display posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from "@angular/core";
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = "angular-sky";
+  title = "angular-blog";
+  posts: Post[] = [
+    {
+      title: "My hobbies",
+      text: "I like travelling and taking pictures",
+      id: 1
+    },
+    { title: "My places", text: "My favorite place is Paris", id: 2 }
+  ];
 }
